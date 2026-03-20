ab3cead786dcd36ee275b7798595abc400fe07f5
1234567Yang <Yang2906455468@gmail.com>
2026-03-20 01:27:43 -0700

    root running browser

--- src/screenshot.ts
+++ src/screenshot.ts
@@ -1,10 +1,12 @@
 import puppeteer from 'puppeteer';
 export async function screenshot(url: string): Promise<Buffer> {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'networkidle2' });
     const image = Buffer.from(await page.screenshot());
-    
-    
+
+
     await browser.close(); return image;
 }
