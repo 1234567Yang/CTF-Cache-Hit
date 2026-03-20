6d1a62a07d35b8abaeeb704f897d0354d55428bd
1234567Yang <Yang2906455468@gmail.com>
2026-03-20 01:36:29 -0700

    screenshot save ram

--- src/screenshot.ts
+++ src/screenshot.ts
@@ -1,8 +1,18 @@
 import puppeteer from 'puppeteer';
 export async function screenshot(url: string): Promise<Buffer> {
     const browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    })
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--single-process',
+            '--no-zygote',
+            '--disable-extensions',
+            '--disable-background-networking',
+            '--disable-software-rasterizer'
+        ]
+    });
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'networkidle2' });
     const image = Buffer.from(await page.screenshot());
